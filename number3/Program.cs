0b3cd870705cff1c569bdeb588398b9d2180e4a3
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-17 22:20:09 +0300

    задача3

---
+++ number3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 % num2 == 0)
+{
+    System.Console.WriteLine("кратно");
+}
+else
+{
+    System.Console.WriteLine($"не кратно, остаток {num1 % num2}");
+}
