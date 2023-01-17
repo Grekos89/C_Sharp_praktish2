7212064a12319d54f6899d09df22d7c1e1ab4daf
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-17 20:18:12 +0300

    задача1

---
+++ number1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 25 => 5
+
+int num  = new Random().Next(10, 100);
+
+Console.WriteLine(num);
+
+int firstNum = num / 10;
+int secondNum = num % 10;
+
+if(firstNum > secondNum)
+{
+    System.Console.WriteLine(firstNum);
+}
+else
+{
+    System.Console.WriteLine(secondNum);
+}
